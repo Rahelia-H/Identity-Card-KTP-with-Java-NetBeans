f78a900a9cd0085cc165a647a89b60247eee3b53
Rahelia Hidayat <raheliahidayat19@gmail.com>
2024-02-26 06:27:07 +0700

    First commit java

---
+++ src/multiforms/multiforms.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiforms;
+
+/**
+ *
+ * @author rahelia H
+ */
+public class multiforms {
+    
+    public static void main(String[] args) {
+        
+        freminput n = new freminput();
+        n.setVisible(true);
+    }
+    
+}
